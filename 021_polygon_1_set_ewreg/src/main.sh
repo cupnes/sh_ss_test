907719bb3fe6e9309e23f5dc07bbce5d716052c7
Yuma Ohgami <yuma@ohgami.jp>
2021-02-21 14:14:57 +0900

    add 021_polygon_1_set_ewreg

---
+++ 021_polygon_1_set_ewreg/src/main.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+# set -uex
+set -ue
+
+. include/common.sh
+. include/sh2.sh
+. include/lib.sh
+. include/ss.sh
+. include/vdp1.sh
+
+main() {
+	# VDP2のシステムレジスタ設定
+	## TVMD
+	## - DISP(b15) = 0
+	## - BDCLMD(b8) = 0
+	## - LSMD(b7-b6) = 0b00
+	## - VRESO(b5-b4) = 0b00
+	## - HRESO(b2-b0) = 0b000
+	copy_to_reg_from_val_long r4 $SS_VDP2_TVMD_ADDR
+	sh2_set_reg r0 00
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r4 r0
+
+	# VDP1のシステムレジスタ設定
+	## TVMR
+	## - VBE(b3) = 0
+	## - TVM(b2-b0) = 0b000
+	copy_to_reg_from_val_long r3 $SS_VDP1_TVMR_ADDR
+	sh2_set_reg r0 00
+	sh2_copy_to_ptr_from_reg_word r3 r0
+	## FBCR(TVMRの2バイト先)
+	sh2_add_to_reg_from_val_byte r3 02
+	sh2_copy_to_ptr_from_reg_word r3 r0
+
+
+	# コマンドテーブル設定
+	local com_adr=$SS_VDP1_VRAM_ADDR
+	vdp1_command_system_clipping_coordinates >src/system_clipping_coordinates.o
+	put_file_to_addr src/system_clipping_coordinates.o $com_adr
+
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_user_clipping_coordinates >src/user_clipping_coordinates.o
+	put_file_to_addr src/user_clipping_coordinates.o $com_adr
+
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_local_coordinates >src/local_coordinates.o
+	put_file_to_addr src/local_coordinates.o $com_adr
+
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_polygon_draw >src/polygon_draw.o
+	put_file_to_addr src/polygon_draw.o $com_adr
+
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_draw_end >src/draw_end.o
+	put_file_to_addr src/draw_end.o $com_adr
+
+	# VDP2のシステムレジスタ設定
+	## TVMD
+	## - DISP(b15) = 1
+	## - BDCLMD(b8) = 1
+	## - LSMD(b7-b6) = 0b00
+	## - VRESO(b5-b4) = 0b00
+	## - HRESO(b2-b0) = 0b000
+	copy_to_reg_from_val_long r4 $SS_VDP2_TVMD_ADDR
+	sh2_set_reg r0 81
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r4 r0
+	## BGON
+	## - N0ON(b0) = 1
+	sh2_add_to_reg_from_val_byte r4 20
+	sh2_set_reg r0 01
+	sh2_copy_to_ptr_from_reg_word r4 r0
+	## PRISA
+	sh2_add_to_reg_from_val_byte r4 68
+	sh2_add_to_reg_from_val_byte r4 68
+	sh2_set_reg r0 06
+	sh2_copy_to_ptr_from_reg_word r4 r0
+
+	# VDP1のシステムレジスタ設定
+	## PTMR(FBCRの2バイト先)
+	sh2_add_to_reg_from_val_byte r3 02
+	## PTM(b1-b0) = 0b10
+	sh2_set_reg r0 02
+	sh2_copy_to_ptr_from_reg_word r3 r0
+	## EWDR(PTMRの2バイト先)
+	sh2_add_to_reg_from_val_byte r3 02
+	sh2_set_reg r0 80
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r3 r0
+	## EWLR(EWDRの2バイト先)
+	sh2_add_to_reg_from_val_byte r3 02
+	sh2_set_reg r0 00
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r3 r0
+	## EWRR(EWLRの2バイト先)
+	sh2_add_to_reg_from_val_byte r3 02
+	sh2_set_reg r0 50
+	sh2_shift_left_logical_8 r0
+	sh2_or_to_r0_from_val_byte df
+	sh2_copy_to_ptr_from_reg_word r3 r0
+
+	infinite_loop
+}
+
+main
