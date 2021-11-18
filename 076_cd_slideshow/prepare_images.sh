3e95e14b46c6f29ef6cbad7b122006b9940d2293
Yuma Ohgami <yuma@ohgami.jp>
2021-11-18 16:48:12 +0900

    076_cd_slideshow: worked on real machines.

--- 076_cd_slideshow/prepare_images.sh
+++ 076_cd_slideshow/prepare_images.sh
@@ -11,11 +11,11 @@ mkdir -p converted_images convert_tmp
 
 n=0
 for orig_img in $IMAGE_LIST; do
-	name=$(printf '%02X' $n)
+	name=$(printf '%02d' $n)
 	page="-page $(identify ${IMAGES_DIR}/$orig_img | cut -d ' ' -f 3)+0+0"
 	convert ${IMAGES_DIR}/$orig_img -strip ${page} -resize 320x224! -depth 5 convert_tmp/${name}.ppm
 	tail -n +4 convert_tmp/${name}.ppm >convert_tmp/${name}.dat
-	cat convert_tmp/${name}.dat | ${CONV_IMG} >converted_images/${name}.img
+	cat convert_tmp/${name}.dat | ${CONV_IMG} >converted_images/${name}
 	n=$((n + 1))
 done
 
