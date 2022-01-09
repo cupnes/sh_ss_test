3ca0f2439205212fc6dd5bbefedd90d1d20a5546
Yuma Ohgami <yuma@ohgami.jp>
2022-01-09 23:29:57 +0900

    add 082_send_key_on
    The reception of the data is shown below.
    
    $ dd if=/dev/snd/midiC1D0 bs=1 | hexdump -Cv
    00000000  90 01 40 90 01 40 90 01  40 90 01 40 90 01 40 90  |..@..@..@..@..@.|
    00000010  01 40 90 01 40 90 01 40  90 01 40 90 01 40 90 01  |.@..@..@..@..@..|
    ...

---
+++ 082_send_key_on/src/main.sh
@@ -0,0 +1,264 @@
+#!/bin/bash
+
+# set -uex
+set -ue
+
+. include/common.sh
+. include/sh2.sh
+. include/lib.sh
+. include/ss.sh
+. include/vdp1.sh
+. include/memmap.sh
+. include/charcode.sh
+. src/vars_map.sh
+. src/funcs_map.sh
+. src/vdp.sh
+. src/con.sh
+
+# このアプリで使用するシェル変数設定
+## スライドショーの画像枚数(10進数で指定)
+NUM_IMGS_DEC=5
+## 最初の画像のFAD(4桁の16進数で指定)
+FAD_FIRST_IMG=02a2
+## 画像間のオフセット[セクタ]
+### 10進数で指定
+SECTORS_IMG_OFS_DEC=70
+### 2桁の16進数で指定
+SECTORS_IMG_OFS=$(extend_digit $(to16 $SECTORS_IMG_OFS_DEC) 2)
+## 最後の画像のFAD(4桁の16進数で指定)
+FAD_LAST_IMG=$(calc16_4 "${FAD_FIRST_IMG}+$(to16 $((SECTORS_IMG_OFS_DEC * (NUM_IMGS_DEC - 1))))")
+
+# コマンドテーブル設定
+# work: r0* - put_file_to_addr,copy_to_reg_from_val_long,この中の作業用
+#     : r1* - put_file_to_addrの作業用
+#     : r2* - put_file_to_addrの作業用
+# ※ *が付いているレジスタはこの関数で書き換えられる
+setup_vram_command_table() {
+	# 05c00000
+	local com_adr=$SS_VDP1_VRAM_ADDR
+	vdp1_command_system_clipping_coordinates >src/system_clipping_coordinates.o
+	put_file_to_addr src/system_clipping_coordinates.o $com_adr
+
+	# 05c00020
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_user_clipping_coordinates >src/user_clipping_coordinates.o
+	put_file_to_addr src/user_clipping_coordinates.o $com_adr
+
+	# 05c00040
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_local_coordinates >src/local_coordinates.o
+	put_file_to_addr src/local_coordinates.o $com_adr
+
+	# con用のVDPCOMの1件目にskip assignを設定
+	## con用のVDPCOMの1件目のアドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CT_CON_BASE
+	## CMDCTRLのJP(ビット14-12)へ設定する値をr0へ設定
+	sh2_set_reg r0 $(two_digits $VDP1_JP_SKIP_ASSIGN)
+	## r0を12ビット左シフト
+	sh2_shift_left_logical_8 r0
+	sh2_shift_left_logical_2 r0
+	sh2_shift_left_logical_2 r0
+	## r0をr1の指す先へ設定
+	sh2_copy_to_ptr_from_reg_word r1 r0
+	## アドレス(r1)を2バイト進める
+	sh2_add_to_reg_from_val_byte r1 02
+	## CMDLINKへ設定する値をr0へ設定
+	sh2_set_reg r0 $(echo $VRAM_CT_OTHER_BASE_CMDLINK | cut -c1-2)
+	sh2_shift_left_logical_8 r0
+	sh2_or_to_r0_from_val_byte $(echo $VRAM_CT_OTHER_BASE_CMDLINK | cut -c3-4)
+	sh2_extend_unsigned_to_reg_from_reg_word r0 r0
+	## r0をr1の指す先へ設定
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# other用のVDPCOMの1件目に描画終了コマンドを配置
+	## other用のVDPCOMの1件目のアドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CT_OTHER_BASE
+	## r1のアドレス先へ描画終了コマンドを配置
+	sh2_set_reg r0 80
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r1 r0
+}
+
+# カラールックアップテーブル設定
+# work: r0* - copy_to_reg_from_val_longの作業用
+#     : r1* - この中の作業用
+# ※ *が付いているレジスタはこの関数で書き換えられる
+setup_vram_color_lookup_table() {
+	# 配置先アドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CLT_BASE
+
+	# | 0 | 透明 | 0x0000 |
+	sh2_xor_to_reg_from_reg r0 r0
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# | 1 | 白   | 0xffff |
+	sh2_add_to_reg_from_val_byte r1 02
+	sh2_set_reg r0 ff
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# | 2 | 透明 | 0x0000 |
+	# | : |  :   |   :    |
+	# | f | 透明 | 0x0000 |
+	sh2_xor_to_reg_from_reg r0 r0
+	local _i
+	for _i in $(seq 2 15); do
+		sh2_add_to_reg_from_val_byte r1 02
+		sh2_copy_to_ptr_from_reg_word r1 r0
+	done
+}
+
+# メイン関数
+main() {
+	# NMI以外の全ての割り込みをマスクする
+	sh2_copy_to_reg_from_sr r0
+	sh2_or_to_r0_from_val_byte $(echo $SH2_SR_BIT_I3210 | cut -c7-8)
+	sh2_copy_to_sr_from_reg r0
+
+	# スタックポインタ(r15)の初期化
+	copy_to_reg_from_val_long r15 $INIT_SP
+
+	# VRAM初期設定
+	setup_vram_command_table
+	setup_vram_color_lookup_table
+
+	# VDP1/2の初期化
+	vdp_init
+
+	# 使用するアドレスをレジスタへ設定しておく
+	copy_to_reg_from_val_long r14 $SS_CT_SND_MOBUF_ADDR
+	copy_to_reg_from_val_long r13 $a_putreg_byte
+	copy_to_reg_from_val_long r7 $SS_CT_SND_MIOSTAT_ADDR
+	copy_to_reg_from_val_long r6 $a_putchar
+
+	# 遅延カウント
+	copy_to_reg_from_val_long r12 00100000
+
+	# キーオンメッセージで送信するキーの値
+	sh2_set_reg r11 01
+
+	(
+		# MIOSTATを取得
+		sh2_copy_to_reg_from_ptr_byte r0 r7
+		# MOFULLビットがセットされているか?
+		sh2_test_r0_and_val_byte $SS_SND_MIOSTAT_BIT_MOFULL
+	) >src/main.3.o
+	sz_3=$(stat -c '%s' src/main.3.o)
+
+	# 無限ループ
+	(
+		# # MOFULL == 0 になるのを待つ
+		# cat src/main.3.o
+		# ## MOFULLビットがセットされていれば(T == 0)、繰り返す
+		# sh2_rel_jump_if_false $(two_comp_d $(((4 + sz_3) / 2)))
+
+		# # 遅延を入れる
+		# sh2_set_reg r0 00
+		# (
+		# 	sh2_add_to_reg_from_val_byte r0 01
+		# 	sh2_compare_reg_gt_reg_unsigned r0 r12
+		# ) >src/main.2.o
+		# cat src/main.2.o
+		# local sz_2=$(stat -c '%s' src/main.2.o)
+		# sh2_rel_jump_if_false $(two_comp_d $(((4 + sz_2) / 2)))
+
+		# # 0xfeを送信
+		# sh2_set_reg r0 fe
+		# sh2_copy_to_ptr_from_reg_byte r14 r0
+
+		# # 0xf8を送信
+		# sh2_set_reg r0 f8
+		# sh2_copy_to_ptr_from_reg_byte r14 r0
+
+		# MOFULL == 0 になるのを待つ
+		cat src/main.3.o
+		## MOFULLビットがセットされていれば(T == 0)、繰り返す
+		sh2_rel_jump_if_false $(two_comp_d $(((4 + sz_3) / 2)))
+
+		# 0x90を送信
+		sh2_set_reg r0 90
+		sh2_copy_to_ptr_from_reg_byte r14 r0
+
+		# MOFULL == 0 になるのを待つ
+		cat src/main.3.o
+		## MOFULLビットがセットされていれば(T == 0)、繰り返す
+		sh2_rel_jump_if_false $(two_comp_d $(((4 + sz_3) / 2)))
+
+		# キー値を送信
+		sh2_copy_to_ptr_from_reg_byte r14 r11
+
+		# MOFULL == 0 になるのを待つ
+		cat src/main.3.o
+		## MOFULLビットがセットされていれば(T == 0)、繰り返す
+		sh2_rel_jump_if_false $(two_comp_d $(((4 + sz_3) / 2)))
+
+		# 0x40を送信
+		sh2_set_reg r0 40
+		sh2_copy_to_ptr_from_reg_byte r14 r0
+
+		# # 書き込んだ1バイトをコンソール出力
+		# sh2_abs_call_to_reg_after_next_inst r13
+		# sh2_copy_to_reg_from_reg r1 r11
+
+		# # 1文字スペースを空ける
+		# sh2_abs_call_to_reg_after_next_inst r6
+		# sh2_set_reg r1 $CHARCODE_SPACE
+	) >src/main.1.o
+	cat src/main.1.o
+	local sz_1=$(stat -c '%s' src/main.1.o)
+	sh2_rel_jump_after_next_inst $(two_comp_3_d $(((4 + sz_1) / 2)))
+	sh2_nop
+}
+
+make_bin() {
+	local file_sz
+	local area_sz
+	local pad_sz
+
+	# メインプログラム領域へジャンプ(32バイト)
+	(
+		copy_to_reg_from_val_long r1 $MAIN_BASE
+		sh2_abs_jump_to_reg_after_next_inst r1
+		sh2_nop
+		sh2_nop	# jmp_main.oのサイズを4の倍数にするためのパディング
+	) >src/jmp_main.o
+	cat src/jmp_main.o
+
+	# 変数領域
+	cat src/vars.o
+	file_sz=$(stat -c '%s' src/vars.o)
+	area_sz=$(echo "ibase=16;$FUNCS_BASE - $VARS_BASE" | bc)
+	pad_sz=$((area_sz - file_sz))
+	if [ $pad_sz -lt 0 ]; then
+		echo 'Error: variable area overflow.' >&2
+		exit 1
+	fi
+	cat <<EOF >&2
+[variable area (unit: byte)]
+- size : $area_sz
+- used : $file_sz
+- avail: $pad_sz
+EOF
+	dd if=/dev/zero bs=1 count=$pad_sz
+
+	# 関数領域
+	cat src/funcs.o
+	file_sz=$(stat -c '%s' src/funcs.o)
+	area_sz=$(echo "ibase=16;$MAIN_BASE - $FUNCS_BASE" | bc)
+	pad_sz=$((area_sz - file_sz))
+	if [ $pad_sz -lt 0 ]; then
+		echo 'Error: function area overflow.' >&2
+		exit 1
+	fi
+	cat <<EOF >&2
+[function area (unit: byte)]
+- size : $area_sz
+- used : $file_sz
+- avail: $pad_sz
+EOF
+	dd if=/dev/zero bs=1 count=$pad_sz
+
+	# メインプログラム領域
+	main
+}
+
+make_bin
