08cd9c46f08d0793e3e8ecb5fdd9e5bf55657f51
Yuma Ohgami <yuma@ohgami.jp>
2022-02-12 13:38:32 +0900

    08c_midi_loader4_1: fix dump_long.sh

--- 08c_midi_loader4_1/dump_long.sh
+++ 08c_midi_loader4_1/dump_long.sh
@@ -7,6 +7,6 @@ MIDI_DEV=/dev/snd/midiC1D0
 
 addr=$1
 
-apps/dump_addr_long.sh $addr >dump_addr_long.exe
+apps/dump_addr_long.sh $addr >apps/dump_addr_long.exe
 ../tools/dump_data_packets apps/dump_addr_long.{exe,pkt}
 dd if=apps/dump_addr_long.pkt of=$MIDI_DEV status=none
