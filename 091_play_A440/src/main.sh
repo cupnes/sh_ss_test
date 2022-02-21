5cf2fc8875e5428fac4a8a48e0c64bc5abccbf72
Yuma Ohgami <yuma@ohgami.jp>
2022-02-22 01:29:59 +0900

    add 091_play_A440

---
+++ 091_play_A440/src/main.sh
@@ -0,0 +1,257 @@
+#!/bin/bash
+
+# set -uex
+set -ue
+
+. include/common.sh
+. include/sh2.sh
+. include/lib.sh
+. include/ss.sh
+. include/vdp1.sh
+. include/memmap.sh
+. include/charcode.sh
+. src/vars_map.sh
+. src/funcs_map.sh
+. src/vdp.sh
+. src/con.sh
+
+PCM_DATA_BASE=25A01000
+SQUARE_WAVE_LOW=80
+SQUARE_WAVE_HIGH=7f
+SQUARE_WAVE_PERIOD=64
+SQUARE_WAVE_PERIOD_DEC=$(echo "ibase=16;$SQUARE_WAVE_PERIOD" | bc)
+
+# コマンドテーブル設定
+# work: r0* - put_file_to_addr,copy_to_reg_from_val_long,この中の作業用
+#     : r1* - put_file_to_addrの作業用
+#     : r2* - put_file_to_addrの作業用
+# ※ *が付いているレジスタはこの関数で書き換えられる
+setup_vram_command_table() {
+	# 05c00000
+	local com_adr=$SS_VDP1_VRAM_ADDR
+	vdp1_command_system_clipping_coordinates >src/system_clipping_coordinates.o
+	put_file_to_addr src/system_clipping_coordinates.o $com_adr
+
+	# 05c00020
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_user_clipping_coordinates >src/user_clipping_coordinates.o
+	put_file_to_addr src/user_clipping_coordinates.o $com_adr
+
+	# 05c00040
+	com_adr=$(calc16 "$com_adr+20")
+	vdp1_command_local_coordinates >src/local_coordinates.o
+	put_file_to_addr src/local_coordinates.o $com_adr
+
+	# con用のVDPCOMの1件目にskip assignを設定
+	## con用のVDPCOMの1件目のアドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CT_CON_BASE
+	## CMDCTRLのJP(ビット14-12)へ設定する値をr0へ設定
+	sh2_set_reg r0 $(two_digits $VDP1_JP_SKIP_ASSIGN)
+	## r0を12ビット左シフト
+	sh2_shift_left_logical_8 r0
+	sh2_shift_left_logical_2 r0
+	sh2_shift_left_logical_2 r0
+	## r0をr1の指す先へ設定
+	sh2_copy_to_ptr_from_reg_word r1 r0
+	## アドレス(r1)を2バイト進める
+	sh2_add_to_reg_from_val_byte r1 02
+	## CMDLINKへ設定する値をr0へ設定
+	sh2_set_reg r0 $(echo $VRAM_CT_OTHER_BASE_CMDLINK | cut -c1-2)
+	sh2_shift_left_logical_8 r0
+	sh2_or_to_r0_from_val_byte $(echo $VRAM_CT_OTHER_BASE_CMDLINK | cut -c3-4)
+	sh2_extend_unsigned_to_reg_from_reg_word r0 r0
+	## r0をr1の指す先へ設定
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# other用のVDPCOMの1件目に描画終了コマンドを配置
+	## other用のVDPCOMの1件目のアドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CT_OTHER_BASE
+	## r1のアドレス先へ描画終了コマンドを配置
+	sh2_set_reg r0 80
+	sh2_shift_left_logical_8 r0
+	sh2_copy_to_ptr_from_reg_word r1 r0
+}
+
+# カラールックアップテーブル設定
+# work: r0* - copy_to_reg_from_val_longの作業用
+#     : r1* - この中の作業用
+# ※ *が付いているレジスタはこの関数で書き換えられる
+setup_vram_color_lookup_table() {
+	# 配置先アドレスをr1へ設定
+	copy_to_reg_from_val_long r1 $VRAM_CLT_BASE
+
+	# | 0 | 透明 | 0x0000 |
+	sh2_xor_to_reg_from_reg r0 r0
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# | 1 | 白   | 0xffff |
+	sh2_add_to_reg_from_val_byte r1 02
+	sh2_set_reg r0 ff
+	sh2_copy_to_ptr_from_reg_word r1 r0
+
+	# | 2 | 透明 | 0x0000 |
+	# | : |  :   |   :    |
+	# | f | 透明 | 0x0000 |
+	sh2_xor_to_reg_from_reg r0 r0
+	local _i
+	for _i in $(seq 2 15); do
+		sh2_add_to_reg_from_val_byte r1 02
+		sh2_copy_to_ptr_from_reg_word r1 r0
+	done
+}
+
+# メイン関数
+main() {
+	# NMI以外の全ての割り込みをマスクする
+	sh2_copy_to_reg_from_sr r0
+	sh2_or_to_r0_from_val_byte $(echo $SH2_SR_BIT_I3210 | cut -c7-8)
+	sh2_copy_to_sr_from_reg r0
+
+	# スタックポインタ(r15)の初期化
+	copy_to_reg_from_val_long r15 $INIT_SP
+
+	# VRAM初期設定
+	setup_vram_command_table
+	setup_vram_color_lookup_table
+
+	# VDP1/2の初期化
+	vdp_init
+
+	# PCMデータをサウンドメモリへ配置
+	copy_to_reg_from_val_long r1 $PCM_DATA_BASE
+	copy_to_reg_from_val_word r2 ${SQUARE_WAVE_LOW}${SQUARE_WAVE_LOW}
+	local n=$((SQUARE_WAVE_PERIOD_DEC / 2 / 2))
+	local i
+	for i in $(seq $n); do
+		sh2_copy_to_ptr_from_reg_word r1 r2
+		sh2_add_to_reg_from_val_byte r1 02
+	done
+	copy_to_reg_from_val_word r2 ${SQUARE_WAVE_HIGH}${SQUARE_WAVE_HIGH}
+	for i in $(seq $n); do
+		sh2_copy_to_ptr_from_reg_word r1 r2
+		sh2_add_to_reg_from_val_byte r1 02
+	done
+
+	# コントロールレジスタを設定
+	## SCSP共通制御レジスタ
+	copy_to_reg_from_val_long r1 $SS_CT_SND_COMMONCTR_ADDR
+	### 00H
+	copy_to_reg_from_val_word r2 0208
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	## スロット別制御レジスタ(スロット0)
+	copy_to_reg_from_val_long r1 $SS_CT_SND_SLOTCTR_S0_ADDR
+	### 00H
+	sh2_set_reg r2 30
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 02H
+	sh2_set_reg r2 10
+	sh2_shift_left_logical_8 r2
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 04H
+	sh2_set_reg r2 00
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 06H
+	sh2_set_reg r2 $SQUARE_WAVE_PERIOD
+	sh2_extend_unsigned_to_reg_from_reg_byte r2 r2
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 08H
+	sh2_set_reg r2 00
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 0AH
+	sh2_set_reg r2 3c
+	sh2_shift_left_logical_8 r2
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 0CH
+	copy_to_reg_from_val_word r2 0380
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 0EH
+	sh2_set_reg r2 00
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 10H
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 12H
+	copy_to_reg_from_val_word r2 8108
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 14H
+	sh2_set_reg r2 00
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+	### 16H
+	sh2_set_reg r2 e0
+	sh2_extend_unsigned_to_reg_from_reg_byte r2 r2
+	sh2_shift_left_logical_8 r2
+	sh2_copy_to_ptr_from_reg_word r1 r2
+	sh2_add_to_reg_from_val_byte r1 02
+
+	# KEY_ONにする
+	copy_to_reg_from_val_long r1 $SS_CT_SND_SLOTCTR_S0_ADDR
+	copy_to_reg_from_val_word r2 1830
+	sh2_copy_to_ptr_from_reg_word r1 r2
+
+	# 無限ループ
+	infinite_loop
+}
+
+make_bin() {
+	local file_sz
+	local area_sz
+	local pad_sz
+
+	# メインプログラム領域へジャンプ(32バイト)
+	(
+		copy_to_reg_from_val_long r1 $MAIN_BASE
+		sh2_abs_jump_to_reg_after_next_inst r1
+		sh2_nop
+		sh2_nop	# jmp_main.oのサイズを4の倍数にするためのパディング
+	) >src/jmp_main.o
+	cat src/jmp_main.o
+
+	# 変数領域
+	cat src/vars.o
+	file_sz=$(stat -c '%s' src/vars.o)
+	area_sz=$(echo "ibase=16;$FUNCS_BASE - $VARS_BASE" | bc)
+	pad_sz=$((area_sz - file_sz))
+	if [ $pad_sz -lt 0 ]; then
+		echo 'Error: variable area overflow.' >&2
+		exit 1
+	fi
+	cat <<EOF >&2
+[variable area (unit: byte)]
+- size : $area_sz
+- used : $file_sz
+- avail: $pad_sz
+EOF
+	dd if=/dev/zero bs=1 count=$pad_sz
+
+	# 関数領域
+	cat src/funcs.o
+	file_sz=$(stat -c '%s' src/funcs.o)
+	area_sz=$(echo "ibase=16;$MAIN_BASE - $FUNCS_BASE" | bc)
+	pad_sz=$((area_sz - file_sz))
+	if [ $pad_sz -lt 0 ]; then
+		echo 'Error: function area overflow.' >&2
+		exit 1
+	fi
+	cat <<EOF >&2
+[function area (unit: byte)]
+- size : $area_sz
+- used : $file_sz
+- avail: $pad_sz
+EOF
+	dd if=/dev/zero bs=1 count=$pad_sz
+
+	# メインプログラム領域
+	main
+}
+
+make_bin
